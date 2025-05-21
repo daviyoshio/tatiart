0eb8da168fda7fecf0589f93dd957b4acc31ea6b
daviyoshio <daviyoshio219@gmail.com>
2025-05-21 07:48:18 -0300

    feat: projeto inicial com Procfile para Railway

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author daviyshiy
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Carrega src/main/resources/templates/index.html
+    }
+}
---
+++ src/main/java/br/com/tatiart/TatiartApplication.java
@@ -0,0 +1,13 @@
+package br.com.tatiart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatiartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatiartApplication.class, args);
+	}
+
+}
