b793b6293c25687f4a03366c4f68f8d5f927cba4
daviyoshio <daviyoshio219@gmail.com>
2025-06-06 21:36:21 -0300

    atualiza página de produto com conteúdo dinâmico por categoria

--- src/main/java/br/com/tatiart/config/LoadDatabase.java
+++ src/main/java/br/com/tatiart/config/LoadDatabase.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.tatiart.config;
 
 import br.com.tatiart.model.Produto;
@@ -16,10 +12,9 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(ProdutoRepository repository) {
         return args -> {
-            repository.save(new Produto("Arte 1", "Descrição da arte 1", "arte1.jpg"));
-            repository.save(new Produto("Arte 2", "Descrição da arte 2", "arte2.jpg"));
-            repository.save(new Produto("Arte 3", "Descrição da arte 3", "arte3.jpg"));
+            repository.save(new Produto(null, "Arte 1", "Descrição da arte 1", "arte-1", "arte", "decoracao"));
+            repository.save(new Produto(null, "Arte 2", "Descrição da arte 2", "arte-2", "arte", "decoracao"));
+            repository.save(new Produto(null, "Arte 3", "Descrição da arte 3", "arte-3", "arte", "decoracao"));
         };
     }
 }
-
